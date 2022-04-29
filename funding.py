827933d9d3127b44cacfcd1f79dc90b3b27ea295
Tyler Hess <tylerlhess@gmail.com>
2022-04-29 10:14:16 -0600

    Added in a LOT. Mostly around proxy posting. No promise this won't change drastically without notice on the actual site.

---
+++ funding.py
@@ -0,0 +1,48 @@
+from proxy_utils import create_tag_object
+from serverside import *
+
+ASSET_NAME = "SQUAWKER/AET_REDEMPTION_TOKEN"
+
+def make_tags(rvnrpc=rvn):
+    #search for transfers of 1 to RUbMcZS36hvfj223sRpYmchY4PEbibNcCi
+    utxos = rvn.getaddressutxos({"addresses": ["RUbMcZS36hvfj223sRpYmchY4PEbibNcCi"], "assetName": ASSET_NAME})['result']
+    viable_tags = []
+    for x in utxos:
+        if x["satoshis"] == 100000000:
+            print(x['txid'])
+            raw_txid = rvn.decoderawtransaction(rvn.getrawtransaction(x['txid'])['result'])['result']
+            print(raw_txid)
+            for y in raw_txid['vout']:
+                vout = y["scriptPubKey"]
+                if vout["type"] == "transfer_asset" and vout["asset"]["name"] == ASSET_NAME and vout["asset"][
+                    "amount"] == 1:
+                    kaw = {
+                        "address": vout["addresses"],
+                        "message": vout["asset"]["message"],
+                        "block": raw_txid["locktime"]
+                    }
+                    viable_tags.append(kaw)
+    print(viable_tags)
+    for tag in viable_tags:
+        print(create_tag_object(tag['message']))
+
+        
+def get_funds(address="RUbMcZS36hvfj223sRpYmchY4PEbibNcCi", rvnrpc=rvn):
+    utxos = rvnrpc.getaddressutxos({"addresses": [address]})['result']
+    for x in utxos:
+        raw_txid = rvn.decoderawtransaction(rvn.getrawtransaction(x['txid'])['result'])['result']
+        if tx_added_to_db(raw_txid):
+            for y in raw_txid['vout']:
+                vout = y["scriptPubKey"]
+                if vout["type"] == "transfer_asset" and vout["asset"]["name"] == ASSET_NAME and vout["asset"][
+                    "amount"] == 1:
+                    kaw = {
+                        "address": vout["addresses"],
+                        "message": vout["asset"]["message"],
+                        "block": raw_txid["locktime"]
+                    }
+                    viable_tags.append(kaw)
+        print(viable_tags)
+        for tag in viable_tags:
+            print(create_tag_object(tag['message']))
+
